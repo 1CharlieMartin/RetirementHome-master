e6b58a17e11cc974ff0187b59c7a412fa6030b66
1CharlieMartin <ckm91011@gmail.com>
2022-12-06 21:40:35 -0800

    Tank drive implementation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/ProjectPushBot.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.Projects;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+public class ProjectPushBot extends Project{
+
+    //Sample project file/hardware map
+
+
+    //Setup motors
+
+    public DcMotor backright = null;
+    public DcMotor backleft = null;
+
+    @Override
+    public void init(HardwareMap ahwMap) {
+        //Save reference to Hardware map
+        hwMap = ahwMap;
+
+
+        //Define and Initialize Motors
+        backright = hwMap.dcMotor.get("backright"); //port c0
+        backleft = hwMap.dcMotor.get("backleft"); //port e3
+
+
+        //Setup Motor directions and Encoder settings
+        backright.setDirection(DcMotor.Direction.FORWARD);
+        backleft.setDirection(DcMotor.Direction.REVERSE);
+
+        backright.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backleft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        backright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Set all motors to zero power
+        Stop();
+    }
+
+    public void Stop(){
+
+        backright.setPower(0);
+        backleft.setPower(0);
+
+
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TankDrive.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.Projects.ProjectPushBot;
+
+//import actual project name
+
+
+@TeleOp(name = "TankDrive")
+public class TankDrive extends LinearOpMode {
+    private ProjectPushBot robot = new ProjectPushBot();
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //connects configuration to code
+        robot.init(hardwareMap);
+        waitForStart();
+
+        while(opModeIsActive()) {
+            //driving controls
+            robot.backleft.setPower(-gamepad1.left_stick_y);
+            robot.backright.setPower(-gamepad1.right_stick_y);
+        }
+        robot.backleft.setPower(0);
+        robot.backright.setPower(0);
+
+    }
+}
